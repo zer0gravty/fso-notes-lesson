d6e87db2a5a6be18bda3c6e97b25df2f5bc5b1a4
Frank Vamos, Jr <fvamos@gmail.com>
2021-03-11 09:30:20 -0500

    Tests update

--- utils/config.js
+++ utils/config.js
@@ -5,7 +5,10 @@ const DB_NAME = process.env.DB_NAME;
 const MONGODB_USER = process.env.MONGODB_USER;
 const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
 const MONGODB_URI = process.env.MONGODB_URI;
-const MONGODB_URL = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URI}/${DB_NAME}?retryWrites=true&w=majority`;
+const MONGODB_LOCAL = `mongodb://localhost:27017/${DB_NAME}`;
+const MONGODB_URL = process.env.NODE_ENV !== 'test'
+  ? `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URI}/${DB_NAME}?retryWrites=true&w=majority`
+  : MONGODB_LOCAL;
 
 module.exports = {
   MONGODB_URL,
